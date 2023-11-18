8d65f73abab8629f95e26de0cbd550edd2fba689
EstefanTT <estefantt@gmail.com>
2023-11-18 18:23:49 +0000

    first comit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # addWatermarkOnPhoto
+
+### Settings 
+
+- Your can edit the margins and size of the "evokeheart plate" by openning this file : 
+/../addWatermarkOnPhoto/settings.json
+
+### How to use the program
+
+- Put the pictures in the "photosWithoutWatermark" directory 
+- Open the terminal (black window to write the code). It is in the apps of your computer.
+
+- If you want website pictures, run this command:   node ../addWatermarkOnPhoto/index.js website
+- If you want Instagram pictures, run this command:  node ../addWatermarkOnPhoto/index.js instagram
+
+- After that, the pictures with the watermark are in the "photosWithWatermark" directory.
+- Don't forget to remove the pictures from "photosWithWatermark" & "photosWithoutWatermark" when you finish ;)
---
+++ index.js
@@ -0,0 +1,117 @@
+const Jimp = require('jimp');
+const { readdirSync, existsSync, mkdirSync } = require('fs')
+
+//###########################################################################################################################################
+//####################################################      INSTRUCTIONS      ###############################################################
+//###########################################################################################################################################
+
+//###########################################################################################################################################
+//######################################################      VARIABLES      ################################################################
+//###########################################################################################################################################
+
+appRoot = __dirname.split('addWatermarkOnPhoto')[0] + 'addWatermarkOnPhoto'
+
+const settings = require('./settings.json')
+
+const directoriesPathToCreate = [
+    appRoot + '/photosWithoutWatermark/',
+    appRoot + '/photosWithWatermark/',
+]
+
+const format = process.argv[2]
+
+//###########################################################################################################################################
+//######################################################      FUNCTIONS      ################################################################
+//###########################################################################################################################################
+
+async function addWatermarkToImage(fileName, format) {
+    try {
+
+        // Load the watermark and the target image
+        const watermark = await Jimp.read(appRoot +  '/media/copperPlate.png');
+        const image = await Jimp.read(appRoot + `/photosWithoutWatermark/${fileName}`);
+
+        // Resize the watermark
+        const watermark_newWidth = Math.round(image.bitmap.width * (settings.copperPlatePercentageOfImageWidth / 100));
+        const aspectRatio = watermark.bitmap.width / watermark.bitmap.height;
+        const watermark_newHeight = Math.round(watermark_newWidth / aspectRatio);
+
+        watermark.resize(watermark_newWidth, watermark_newHeight);
+
+        // Calculate position for watermark (bottom right with 20px margin)
+        let x, y
+        if (format === 'website') {
+            x = image.bitmap.width - watermark.bitmap.width - settings.marginRight;
+            y = image.bitmap.height - watermark.bitmap.height - settings.marginBottom;
+        } else if (format === 'instagram') {
+            x = Math.round((image.bitmap.width / 2) - (watermark_newWidth / 2))
+            y = image.bitmap.height - watermark.bitmap.height - settings.marginBottom - settings.extraBottomOnInstagram;
+        }
+
+        // Composite the watermark onto the target image
+        image.composite(watermark, x, y, {
+            mode: Jimp.BLEND_SOURCE_OVER,
+            opacitySource: 1.0,
+            opacityDest: 1.0
+        });
+
+        // Save the image with watermark
+        await image.writeAsync(appRoot + `/photosWithWatermark/${fileName}`);
+    } catch (error) {
+        console.error('An error occurred:', error);
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function createAllNonGitTrackedDirectories(directoriesPathToCreate) {
+
+    directoriesPathToCreate.forEach(directoryPathToCreate => {
+        if (!existsSync(directoryPathToCreate)) {
+            mkdirSync(directoryPathToCreate)
+        }
+    })
+}
+
+//###########################################################################################################################################
+//########################################################      Launch      #################################################################
+//###########################################################################################################################################
+
+// Example of iterating over filenames (replace with your actual filenames)
+// const fileNames = ['image1.jpg', 'image2.png']; // Replace with your actual file names array
+// fileNames.forEach(fileName => addWatermarkToImage(fileName));
+
+function launch() {
+
+    createAllNonGitTrackedDirectories(directoriesPathToCreate)
+
+    // Warning
+    if (format !== 'website' && format !== 'instagram') {
+        console.log("The command you enter is not valid, is 'website' or 'instagram' written correctly ?")
+        return
+    }
+
+    const files = readdirSync(appRoot + '/photosWithoutWatermark')
+
+    files.forEach(fileName => {
+        addWatermarkToImage(fileName, format)
+    })
+}
+
+launch()
+
+
+/*###########################################################################################################################################
+//################################################       NOTES, OBJECTS & JSON       ########################################################
+//###########################################################################################################################################
+
+
+
+//###########################################################################################################################################
+//#####################################################       END       #####################################################################
+//#########################################################################################################################################*/
+
